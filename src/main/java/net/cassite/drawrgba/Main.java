53032df2f00a202cc1cebb45f3124f4164616e73
wkgcass <wkgcass@hotmail.com>
2023-12-17 15:23:58 +0800

    make it compatible with jlink

--- src/main/java/net/cassite/drawrgba/Main.java
+++ src/main/java/net/cassite/drawrgba/Main.java
@@ -1,5 +1,8 @@
 package net.cassite.drawrgba;
 
+import io.vproxy.base.util.LogType;
+import io.vproxy.base.util.Logger;
+import io.vproxy.vfx.control.globalscreen.GlobalScreenUtils;
 import javafx.application.Application;
 
 public class Main {
@@ -9,6 +12,15 @@ public class Main {
         if (args.length >= 1 && args[0].equals("version")) {
             System.out.println(VERSION);
             return;
+        }
+        {
+            var dllPath = "/net/cassite/drawrgba/JNativeHook_x64.dll";
+            var dllStream = Main.class.getResourceAsStream(dllPath);
+            if (dllStream == null) {
+                Logger.error(LogType.SYS_ERROR, STR."\{dllPath} not found, program might not work");
+            } else {
+                GlobalScreenUtils.releaseJNativeHookNativeToLibraryPath(dllStream);
+            }
         }
         Application.launch(FXMain.class);
     }
